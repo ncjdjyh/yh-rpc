c6349379e1bfdcd63eaf26dae92d7a61edf19311
蒋元昊 <yhjiang@che300.com>
2022-08-18 08:10:52 +0800

    update code

--- src/main/java/com/neo/yhrpc/provider/DefaultHandler.java
+++ src/main/java/com/neo/yhrpc/provider/DefaultHandler.java
@@ -5,7 +5,6 @@ import com.neo.yhrpc.common.MessageInput;
 import io.netty.channel.ChannelHandlerContext;
 import org.springframework.stereotype.Component;
 
-@Component
 public class DefaultHandler implements IMessageHandler<MessageInput> {
 	@Override
 	public void handle(ChannelHandlerContext ctx, String requesetId, MessageInput input) {
--- src/test/java/util/NacosTest.java
+++ src/test/java/util/NacosTest.java
@@ -1,14 +1,13 @@
 package util;
 
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingFactory;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.neo.yhrpc.common.NacosNamingService;
+import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.annotation.Order;
 
 import java.util.List;
 
@@ -16,10 +15,12 @@ import java.util.List;
  * @author ncjdjyh
  * @since 2022/8/17
  */
-@SpringBootTest
 public class NacosTest {
     @Test
     public void registerInstance() throws NacosException {
+        NamingService instance = NacosNamingService.getInstance();
+        Assert.assertNotNull(instance);
+
         NamingService namingService = NamingFactory.createNamingService("127.0.0.1:8848");
 
         namingService.registerInstance("fib", "localhost", 8080);
